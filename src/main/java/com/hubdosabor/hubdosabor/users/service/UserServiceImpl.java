79e5f716e1804a98bbbf47b922fdf9efc3318f79
llyndev <weslley.alves3310@gmail.com>
2025-10-15 14:33:21 -0300

    feat: integrate CEP service for address handling during user registration

--- src/main/java/com/hubdosabor/hubdosabor/users/service/UserServiceImpl.java
+++ src/main/java/com/hubdosabor/hubdosabor/users/service/UserServiceImpl.java
@@ -1,6 +1,9 @@
 package com.hubdosabor.hubdosabor.users.service;
 
+import com.hubdosabor.hubdosabor.address.service.CepService;
 import com.hubdosabor.hubdosabor.users.dto.request.RegisterRequest;
+import com.hubdosabor.hubdosabor.address.dto.response.CepResponse;
+import com.hubdosabor.hubdosabor.address.model.Address;
 import com.hubdosabor.hubdosabor.users.model.User;
 import com.hubdosabor.hubdosabor.users.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -13,6 +16,7 @@ public class UserServiceImpl implements RegisterService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final CepService cepService;
 
     public void registerUser(RegisterRequest request) {
         if (userRepository.findByEmailContainingIgnoreCase(request.email()).isPresent()) {
@@ -23,9 +27,21 @@ public class UserServiceImpl implements RegisterService {
             throw new IllegalStateException("Invalid credentials.");
         }
 
+        CepResponse cepResponse = cepService.getCep(request.cep());
+
+        Address address = new Address();
+        address.setCep(cepResponse.cep());
+        address.setLogradouro(cepResponse.logradouro());
+        address.setComplemento(cepResponse.complemento());
+        address.setBairro(cepResponse.bairro());
+        address.setNumero(request.number());
+        address.setLocalidade(cepResponse.cidade());
+        address.setUf(cepResponse.estado());
+
         User user = new User();
         user.setName(request.name());
         user.setEmail(request.email());
+        user.setAddress(address);
         user.setTelephone(request.telephone());
         user.setRole(User.Role.USER);
         user.setPassword(passwordEncoder.encode(request.password()));
